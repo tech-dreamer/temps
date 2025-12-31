caaf845023e1e17f14e6f3c4c67927e3d575fcfd
cameronlattz <cameronlattz@gmail.com>
2025-12-30 22:19:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let cities = [];
 async function loadCities() {
   const { data, error } = await client
     .from('cities')
-    .select('id, name, timezone')
+    .select('id, name, timezone_id')
     .order('name');
 
   if (error || !data) {
